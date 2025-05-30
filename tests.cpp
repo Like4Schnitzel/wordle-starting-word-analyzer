51876c9c8eada5417fdd0870f56749fdbd396bd1
Laura <rw21062006@gmail.com>
2025-05-30 15:19:35 +0200

    fix tests tabs

--- tests.cpp
+++ tests.cpp
@@ -3,17 +3,17 @@
 
 int main()
 {
-   assert(overlappingLetters("APPLE", "PEARS") == true);
-   assert(overlappingLetters("APPLE", "BINGO") == false);
+    assert(overlappingLetters("APPLE", "PEARS") == true);
+    assert(overlappingLetters("APPLE", "BINGO") == false);
 
-   assert(wordToNumber("APPLE") == 0b00000000000000001000100000010001);
+    assert(wordToNumber("APPLE") == 0b00000000000000001000100000010001);
 
-   assert(overlappingLettersWithNumbers(wordToNumber("APPLE"), wordToNumber("PEARS")) == true);
-   assert(overlappingLettersWithNumbers(wordToNumber("APPLE"), wordToNumber("BINGO")) == false);
+    assert(overlappingLettersWithNumbers(wordToNumber("APPLE"), wordToNumber("PEARS")) == true);
+    assert(overlappingLettersWithNumbers(wordToNumber("APPLE"), wordToNumber("BINGO")) == false);
 
-   assert(mostFrequentLetterInWord(wordToNumber("APPLE")) == 'E');
-   assert(mostFrequentLetterInWord(wordToNumber("BOBBY")) == 'O');
-   
-   std::cout << "All tests passed ^-^\n";
-   return 0;
+    assert(mostFrequentLetterInWord(wordToNumber("APPLE")) == 'E');
+    assert(mostFrequentLetterInWord(wordToNumber("BOBBY")) == 'O');
+
+    std::cout << "All tests passed ^-^\n";
+    return 0;
 }
